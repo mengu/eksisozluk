a29a699de7387201343a5cfd46059fd5b7a88af3
mengu <mengukagan@gmail.com>
2010-10-17 21:02:05 +0300

    imported models.

--- eksisozluk/model/__init__.py
+++ eksisozluk/model/__init__.py
@@ -1,6 +1,8 @@
 """The application's model objects"""
 from eksisozluk.model.meta import Session, Base
-
+from eksisozluk.model.entry import Topic, Entry, Vote
+from eksisozluk.model.message import Message
+from eksisozluk.model.user import User
 
 def init_model(engine):
     """Call me before using any of the tables or classes in the model"""
